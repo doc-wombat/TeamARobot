ded771e1a2fcb21a835f1b636d6b0c2824b025d6
Brendan Donohue <brendandonohue2022@gmail.com>
2021-10-27 16:00:57 -0700

    Add project files.

---
+++ Team A Robot Code/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using System;
+using System.Threading;
+
+using CTRE.Phoenix;
+using CTRE.Phoenix.Controller;
+using CTRE.Phoenix.MotorControl;
+using CTRE.Phoenix.MotorControl.CAN;
+
+namespace fartbutt
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GameController gamepad = new GameController(UsbHostDevice.GetInstance(0));
+            TalonSRX motor = new TalonSRX(0);
+            motor.ConfigFactoryDefault();
+
+            while (true)
+            {
+                CTRE.Phoenix.Watchdog.Feed();
+                motor.Set(ControlMode.PercentOutput, gamepad.GetAxis(0));
+                System.Threading.Thread.Sleep(20);
+            }
+        }
+    }
+}
