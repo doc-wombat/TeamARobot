94a7db835f355ce25aec30a22b973e7c51695c02
Brendan Donohue <brendandonohue2022@gmail.com>
2021-11-06 10:57:09 -0700

    Added dubiously-functional swingBat function

--- Team A Robot Code/Program.cs
+++ Team A Robot Code/Program.cs
@@ -13,22 +13,28 @@ namespace TeamA
     public class Program
     {
         static GameController gamepad = new GameController(UsbHostDevice.GetInstance(0));
-        static TalonSRX leftMotor = new TalonSRX(0);
-        static TalonSRX rightMotor = new TalonSRX(1);
+        //static TalonSRX leftMotor = new TalonSRX(0);
+        //static TalonSRX rightMotor = new TalonSRX(1);
+        static TalonSRX bat = new TalonSRX(2);
         public static void Main()
         {
-            leftMotor.ConfigFactoryDefault();
-            rightMotor.ConfigFactoryDefault();
+            //leftMotor.ConfigFactoryDefault();
+            //rightMotor.ConfigFactoryDefault();
+            bat.ConfigFactoryDefault();
 
             while (true)
             {
                 CTRE.Phoenix.Watchdog.Feed();
                 drive();
+                if (gamepad.GetButton(1))
+                {
+                    swingBat();
+                }
                 System.Threading.Thread.Sleep(20);
             }
         }
 
-        /* Values within 10% of 0 get ignored (Controller deadzone) */
+        // Values within 10% of 0 get ignored (Controller deadzone)
         public static void deadzone(double axis)
         {
             if ((axis > .1) && (axis < -.1))
@@ -58,8 +64,39 @@ namespace TeamA
             double leftThrot = y + twist;
             double rightThrot = y - twist;
 
-            leftMotor.Set(ControlMode.PercentOutput, leftThrot);
-            rightMotor.Set(ControlMode.PercentOutput, rightThrot);
+            //leftMotor.Set(ControlMode.PercentOutput, leftThrot);
+            //rightMotor.Set(ControlMode.PercentOutput, -rightThrot);
+        }
+        
+        /* Rotates motor exactly 90 degrees by rotating for
+         * exactly .25 seconds (1rps) then returns for exactly
+         * .25 seconds
+         * 
+         * Doesn't work very well. Probably will need encoder
+         */
+        public static void swingBat()
+        {
+            long startTime = millis();
+            while ((millis() - startTime) < 250) 
+            {
+                bat.Set(ControlMode.PercentOutput, 1);
+                CTRE.Phoenix.Watchdog.Feed();
+            }
+            bat.Set(ControlMode.PercentOutput, 0);
+            System.Threading.Thread.Sleep(100);
+            startTime = millis();
+            while ((millis() - startTime) < 250)
+            {
+                bat.Set(ControlMode.PercentOutput, -1);
+                CTRE.Phoenix.Watchdog.Feed();
+            }
+            bat.Set(ControlMode.PercentOutput, 0);
+        }
+
+        // Gets current # of milliseconds
+        public static long millis()
+        {
+            return DateTime.Now.Ticks / (TimeSpan.TicksPerMillisecond);
         }
     }
 }
